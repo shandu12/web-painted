ea6496e38cf332dd6a6cc780f4fb501fc8c7e3ef
gadash <gadacha@outlook.it>
2026-01-22 11:14:57 +0100

    add production link for server api

--- src/app/utils/server.ts
+++ src/app/utils/server.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = "http://localhost:3000/api"
+export const server = dev ?  "http://localhost:3000/api" : "https://web-painted-tyza.vercel.app/api";
